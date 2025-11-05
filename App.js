c7be9a84a21af65364287ad75bb6d96892b22d9c
vnaum0 <vnaumenko@alum.joviat.cat>
2025-11-05 09:15:14 +0100

    Commit del switch (no fer merge)
    IMPORTANT
    No fer merge amb main fins tenir completat les pàgines Mapa i llistat.
    
    El switch esta incorporat en el App.js.

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import SegmentedControlTab from 'react-native-segmented-control-tab';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Mapa from './src/screens/Mapa';
+import Llista from './src/screens/Llista';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleIndexChange = (index, navigation) => {
+    setSelectedIndex(index);
+    navigation.navigate(index === 0 ? "Mapa" : "Llista");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={({ navigation }) => ({
+          headerTitle: () => (
+            <View style={{ width: 200 }}>
+              <SegmentedControlTab
+                values={['Map', 'List']}
+                selectedIndex={selectedIndex}
+                onTabPress={(index) => handleIndexChange(index, navigation)}
+                borderRadius={20}
+                tabsContainerStyle={{ marginTop: 8 }}
+              />
+            </View>
+          ),
+        })}
+      >
+        <Stack.Screen name="Mapa" component={Mapa} />
+        <Stack.Screen name="Llista" component={Llista} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
