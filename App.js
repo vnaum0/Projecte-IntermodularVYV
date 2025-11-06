63c7ba6257e54d92f0349f1de2e67f325bd177a4
vnaum0 <vnaumenko@alum.joviat.cat>
2025-11-06 10:56:21 +0100

    Actualització del switch

--- App.js
+++ App.js
@@ -1,42 +1,33 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
 import SegmentedControlTab from 'react-native-segmented-control-tab';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Mapa from './src/screens/Mapa';
 import Llista from './src/screens/Llista';
-
-const Stack = createNativeStackNavigator();
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
-  const handleIndexChange = (index, navigation) => {
-    setSelectedIndex(index);
-    navigation.navigate(index === 0 ? "Mapa" : "Llista");
-  };
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={({ navigation }) => ({
-          headerTitle: () => (
-            <View style={{ width: 200 }}>
-              <SegmentedControlTab
-                values={['Map', 'List']}
-                selectedIndex={selectedIndex}
-                onTabPress={(index) => handleIndexChange(index, navigation)}
-                borderRadius={20}
-                tabsContainerStyle={{ marginTop: 8 }}
-              />
-            </View>
-          ),
-        })}
-      >
-        <Stack.Screen name="Mapa" component={Mapa} />
-        <Stack.Screen name="Llista" component={Llista} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ alignItems: 'center', marginTop: 15, marginBottom: 15, }}>
+        <SegmentedControlTab
+          values={['Map', 'List']}
+          selectedIndex={selectedIndex}
+          onTabPress={(index) => setSelectedIndex(index)}
+          borderRadius={20}
+          tabsContainerStyle={{ width: 300, backgroundColor: '#E6E6E6', borderRadius: 20, padding: 6 }}
+          tabStyle={{ borderColor: 'transparent', backgroundColor: 'transparent'}}
+          activeTabStyle={{ backgroundColor: '#FFFFFF', borderRaddius: 18 }}
+          tabTextStyle={{ color: '#000000', fontWeight: 500}}
+          activeTabTextStyle={{ color: '#000000', fontWeight: 600 }}
+        />
+      </View>
+
+      <View style={{ flex: 1 }}>
+        {selectedIndex === 0 ? <Mapa /> : <Llista />}
+      </View>
+    </SafeAreaView>
   );
 }
